1749c01eea8e7e7bc3f258f1ff2528555bce0e29
Timo <timotheekabore79@gmail.com>
2025-07-31 02:58:13 +0000

    ajout des fichiers pour le ssl

--- vote/app.py
+++ vote/app.py
@@ -48,4 +48,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True, ssl_context=('server.crt', 'server.key'))
+    app.run(host='0.0.0.0', port=443, debug=True, threaded=True, ssl_context=('server.crt', 'server.key'))
